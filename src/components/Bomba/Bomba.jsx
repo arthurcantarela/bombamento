be6bec6c21313e4415935fd9ed06def3a49e501e
Arthur Cantarela <arthur.cantarela@clickideia.com.br>
2018-06-18 16:57:35 -0300

    Slider

--- src/components/Bomba/Bomba.jsx
+++ src/components/Bomba/Bomba.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Bomba.scss";
 
 const Bomba = ({ code, credits, year, semester, grade, ...rest }) => (
-  <li>
+  <li style={{ width: "100px" }}>
     <h1>{code}</h1>
     <h2>{credits}</h2>
     {year} - {semester} - {grade}
--- src/views/Resultado/Resultado.jsx
+++ src/views/Resultado/Resultado.jsx
@@ -1,11 +1,25 @@
 import React, { Fragment } from "react";
 import "./Resultado.scss";
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
 import { Bomba } from "components";
 
 const Resultado = ({ bombas, redo, ...rest }) => (
   <Fragment>
-    <ul>{bombas.map((bomba, i) => <Bomba key={i} {...bomba} />)}</ul>
+    <ul class="BombaSlider">
+      <Slider
+        settings={{
+          dots: true,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }}
+      >
+        {bombas.map((bomba, i) => <Bomba key={i} {...bomba} />)}
+      </Slider>
+    </ul>
     <button onClick={redo}>Refazer</button>
   </Fragment>
 );
