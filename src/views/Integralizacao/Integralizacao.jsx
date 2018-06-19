9102b4a25e258779fe2b26a03f09dde734c01dbe
Arthur Cantarela <arthur.cantarela@clickideia.com.br>
2018-06-19 11:24:08 -0300

    Styling textarea

--- src/views/Integralizacao/Integralizacao.jsx
+++ src/views/Integralizacao/Integralizacao.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faBomb from "@fortawesome/fontawesome-free-solid/faBomb";
 import "./Integralizacao.scss";
 
 const Integralizacao = ({ onChange, value, calculate, ...rest }) => (
   <React.Fragment>
-    <div>
-      <textarea spellCheck={false} {...{ onChange, value }} />
+    <div className="Integralizacao">
+      {value ? null : (
+        <h1
+          className="IntegralizacaoPlaceholder"
+          onClick={() => this.textarea.focus()}
+        >
+          Cole aqui sua integralização
+        </h1>
+      )}
+      <div className="IntegralizacaoTextWrap">
+        <textarea
+          ref={textarea => (this.textarea = textarea)}
+          className="IntegralizacaoText"
+          spellCheck={false}
+          {...{ onChange, value }}
+        />
+      </div>
+      {value ? (
+        <button className="IntegralizacaoBtn" onClick={calculate}>
+          <FontAwesomeIcon icon={faBomb} />
+        </button>
+      ) : null}
     </div>
-    <button onClick={calculate}>Calcular</button>
   </React.Fragment>
 );
 
